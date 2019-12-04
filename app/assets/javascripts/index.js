bf3acaf8b16b5d3d22014f9a3564942d8289559e
woody0824 <woody5232@gmail.com>
2019-12-04 19:37:48 +0900

    test

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,6 @@
 $(function() {
     console.log('ausv');
-  $('.header__menu-right__mypage').hover(
+  $('.header__menu-right--mypage').hover(
     function() {
         //マウスカーソルが重なった時の処理
         $('.my-menu').css('display', 'block');
