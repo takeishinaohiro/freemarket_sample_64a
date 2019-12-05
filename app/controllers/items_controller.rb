24891b148bffd4b91ebdffe8ddb0b31d037ca48e
yuma11 <11yuma20@gmail.com>
2019-12-05 18:51:23 +0900

    ビューの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,7 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def sell
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "items#index"
+
+  get "/sell", to: 'items#sell', as: 'sell'
 end
