0cc068cd244e15961a0076957276fbcd1b7e9e83
woody0824 <woody5232@gmail.com>
2019-12-06 13:40:24 +0900

    マイページ

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,3 +9,14 @@ $(function() {
     }
     );
 });
+$(function($){
+  $('.mypage-tab').click(function(){
+      $('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+      $('.is-show').removeClass('is-show');
+      // クリックしたタブからインデックス番号を取得
+      const index = $(this).index();
+      // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+      $('.mypage__middle--content').eq(index).addClass('is-show');
+  });
+});
