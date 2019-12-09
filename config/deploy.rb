c0a5a08cbd2ce9ab3e962af93ee0c350a8e8f280
jsan105 <57351081+jsan105@users.noreply.github.com>
2019-12-09 21:50:11 +0900

    s3アップロード

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,9 @@ set :application, 'freemarket_sample_64a'
 set :repo_url,  'git@github.com:yuma11/freemarket_sample_64a.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
@@ -51,3 +53,4 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
+
