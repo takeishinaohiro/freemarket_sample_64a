ef48a7eebf0173a1aaace33bfdd9d08de3171acd
jsan105 <57351081+jsan105@users.noreply.github.com>
2019-12-09 17:11:35 +0900

    s3記述変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
