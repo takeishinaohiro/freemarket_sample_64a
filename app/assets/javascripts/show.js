5e9a904f0da2a908b36c10e4728c09a61a25022f
takeishinaohiro <rurushuviburitaniaga214@gmail.com>
2019-12-10 15:03:03 +0900

    slick

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,17 +1,55 @@
 $(function() {
-  $('.slider-nav').slick({
+  // 詳細ページのslick上
+  $('.imagemain').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.slider-nav'
+    asNavFor: '.imagesab'
   });
+
+    // 詳細ページのslick下
   $('.imagesab').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    asNavFor: '.slider-for',
+    asNavFor: '.imagemain',
     dots: true,
     centerMode: true,
     focusOnSelect: true
   });
 })
+
+
+$(function() {
+  // 写真のopacity
+  $('.imagesab-a').click(function(){
+    console.log("apa");
+    $('.imagesab-b').addClass('none')
+    $('.imagesab-c').addClass('none')   
+    $('.imagesab-d').addClass('none')
+    $('.imagesab-a').removeClass('none')
+  })
+
+  $('.imagesab-b').click(function(){
+    console.log("apa");
+    $('.imagesab-a').addClass('none')
+    $('.imagesab-c').addClass('none')   
+    $('.imagesab-d').addClass('none')
+    $('.imagesab-b').removeClass('none')
+  })
+  $('.imagesab-c').click(function(){
+    console.log("apa");
+    $('.imagesab-a').addClass('none')
+    $('.imagesab-b').addClass('none')   
+    $('.imagesab-d').addClass('none')
+    $('.imagesab-c').removeClass('none')
+  })
+  $('.imagesab-d').click(function(){
+    console.log("apa");
+    $('.imagesab-a').addClass('none')
+    $('.imagesab-b').addClass('none')   
+    $('.imagesab-c').addClass('none')
+    $('.imagesab-d').removeClass('none')
+  })
+
+});
