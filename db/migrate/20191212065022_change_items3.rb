d84a608216661b3d63b7ebf8ad250996f6826bbf
takeishinaohiro <rurushuviburitaniaga214@gmail.com>
2019-12-13 11:49:04 +0900

    変更

--- db/migrate/20191212065022_change_items3.rb
+++
@@ -1,8 +0,0 @@
-class ChangeItems3 < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :buyers, :items_id
-    remove_column :sellers , :items_id
-    add_column :sellers, :item_id,:string
-    add_column :buyers, :item_id,:string
-  end
-end
