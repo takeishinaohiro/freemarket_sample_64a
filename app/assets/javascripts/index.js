1bc6a7151bac1e5b67a970d80b41811493c7e028
yuma11 <11yuma20@gmail.com>
2019-12-16 13:59:01 +0900

    ログアウト機能

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,91 +1,94 @@
 $(function() {
-
-  $('.sidebar__top--item').hover(
+  $(".sidebar__top--item").hover(
     function() {
-      $(this).find('.fa-chevron-right').css({'color':'gray', 'transform':'translate(5px, -50%)'});
+      $(this)
+        .find(".fa-chevron-right")
+        .css({ color: "gray", transform: "translate(5px, -50%)" });
     },
     function() {
-      $(this).find('.fa-chevron-right').css({'color':'lightgray', 'transform':'translate(0, -50%)'});
+      $(this)
+        .find(".fa-chevron-right")
+        .css({ color: "lightgray", transform: "translate(0, -50%)" });
     }
   );
 
-$(function($){
-  $('.a').click(function(){
-      $('.b').removeClass('is-active');
-      $(this).addClass('is-active');
-      $('.f').removeClass('is-show');
+  $(function($) {
+    $(".a").click(function() {
+      $(".b").removeClass("is-active");
+      $(this).addClass("is-active");
+      $(".f").removeClass("is-show");
       // クリックしたタブからインデックス番号を取得
       const index = $(this).index();
       // クリックしたタブと同じインデックス番号をもつコンテンツを表示
-      $('.e').addClass('is-show');
+      $(".e").addClass("is-show");
+    });
   });
-});
 
-$(function($){
-  $('.b').click(function(){
-      $('.a').removeClass('is-active');
-      $(this).addClass('is-active');
-      $('.e').removeClass('is-show');
+  $(function($) {
+    $(".b").click(function() {
+      $(".a").removeClass("is-active");
+      $(this).addClass("is-active");
+      $(".e").removeClass("is-show");
       // クリックしたタブからインデックス番号を取得
       const index = $(this).index();
       // クリックしたタブと同じインデックス番号をもつコンテンツを表示
-      $('.f').addClass('is-show');
+      $(".f").addClass("is-show");
+    });
   });
-});
 
-$(function($){
-  $('.c').click(function(){
-      $('.d').removeClass('is-active');
-      $(this).addClass('is-active');
-      $('.h').removeClass('is-show');
+  $(function($) {
+    $(".c").click(function() {
+      $(".d").removeClass("is-active");
+      $(this).addClass("is-active");
+      $(".h").removeClass("is-show");
       // クリックしたタブからインデックス番号を取得
       const index = $(this).index();
       // クリックしたタブと同じインデックス番号をもつコンテンツを表示
-      $('.g').addClass('is-show');
+      $(".g").addClass("is-show");
+    });
   });
-});
 
-$(function($){
-  $('.d').click(function(){
-      $('.c').removeClass('is-active');
-      $(this).addClass('is-active');
-      $('.g').removeClass('is-show');
+  $(function($) {
+    $(".d").click(function() {
+      $(".c").removeClass("is-active");
+      $(this).addClass("is-active");
+      $(".g").removeClass("is-show");
       // クリックしたタブからインデックス番号を取得
       const index = $(this).index();
       // クリックしたタブと同じインデックス番号をもつコンテンツを表示
-      $('.h').addClass('is-show');
-  });
-});
-
-
-// トップページのカルーセル
-$(function(){
-  $('.slider').slick({
-    prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
-    nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    speed: 800
+      $(".h").addClass("is-show");
+    });
   });
 
-  $('.slick-dots li').on('mouseover', function() {
-    $('.slider').slick('goTo', $(this).index());
+  // トップページのカルーセル
+  $(function() {
+    $(".slider").slick({
+      prevArrow: '<i class="fa fa-angle-left arrow arrow-left"></i>',
+      nextArrow: '<i class="fa fa-angle-right arrow arrow-right"></i>',
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 800
+    });
+
+    $(".slick-dots li").on("mouseover", function() {
+      $(".slider").slick("goTo", $(this).index());
+    });
   });
-});
+
+  $(function() {
+    $(".header__menu-right--mypage,.my-menu").hover(
+      function() {
+        //マウスカーソルが重なった時の処理
+        $(".my-menu").css("display", "block");
+      },
+      function() {
+        //マウスカーソルが離れた時の処理
+        $(".my-menu").css("display", "none");
+      }
+    );
+  });
 
 
-$(function() {
-$('.header__menu-right--mypage').hover(
-  function() {
-      //マウスカーソルが重なった時の処理
-      $('.my-menu').css('display', 'block');
-  },
-  function() {
-      //マウスカーソルが離れた時の処理
-      $('.my-menu').css('display', 'none');
-  }
-  );
-});
 
 });
