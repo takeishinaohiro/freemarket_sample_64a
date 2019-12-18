aa598dee68dc67e083d04f71142ae2325effd47f
woody0824 <woody5232@gmail.com>
2019-12-18 13:26:29 +0900

    軽微な修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
-  # devise_for :users, controllers: {
-  #   omniauth_callbacks: "users/omniauth_callbacks"
-  # }
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   get  '/auth/:provider/callback', to: "items#new"
   root 'items#index'
 
