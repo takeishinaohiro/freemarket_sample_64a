f29976ac59570df2f8ffb757636f2d31e1f93f4a
takeishinaohiro <rurushuviburitaniaga214@gmail.com>
2019-12-18 14:54:49 +0900

    マージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base  
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
     new_address_path
   end
-  
+
+  def after_inactive_sign_up_path_for(resource)
+    new_address_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  
-
 end
