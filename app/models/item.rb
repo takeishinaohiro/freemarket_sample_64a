520414082975306b6fb79781dc353478c1c7a572
woody0824 <woody5232@gmail.com>
2019-12-19 16:10:34 +0900

    ユーザーid付与

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-
+  belongs_to :user
   has_many :images
   has_one :buyer
   has_one :seller
